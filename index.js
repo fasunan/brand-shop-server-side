7622a54bd9e5753b1e334ec12dd0385f101eb4f9
fasunan <fasunan@gmail.com>
2023-10-19 01:38:50 +0600

    rrcv data from UI, and also send it back, but one API is not working

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
+// require('dotenv').config();
 
 // middle were 
 app.use(express.json());
 app.use(cors());
 
-const port= process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
+
+// console.log(process.env.DB_USER)
+// console.log(process.env.DB_PASS)
 
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://fasunan:klTug7Ls0pcfWwhR@cluster0.2gzcwih.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,6 +29,31 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productCollection = client.db('productDB').collection('product');
+
+
+    app.get('/product', async (req, res) => {
+      const cursor = productCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/product/:name', async (req, res) => {
+      const name = req.params.brandName;
+      const query = { brandName: name };
+      const cursor =  productCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post('/product', async (req, res) => {
+      const allProducts = req.body;
+      console.log(allProducts)
+      const result = await productCollection.insertOne(allProducts);
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -38,10 +66,10 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Server is running')
-  })
+  res.send('Server is running')
+})
 
 
-  app.listen(port,()=>{
-    console.log(`app in running on port ${port}`)
-  })
+app.listen(port, () => {
+  console.log(`app in running on port ${port}`)
+})
